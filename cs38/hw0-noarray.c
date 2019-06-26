88616b3702285582805cf6c413e890164380bf52
Will Hoback <will.hoback@gmail.com>
2019-06-25 22:09:18 -0400

    format

--- cs38/hw0-noarray.c
+++ cs38/hw0-noarray.c
@@ -18,38 +18,38 @@ static int i = 1;
 
 int main()
 {
-
-        //prompt user for positve int
-        while(i == 1)
+    
+    //prompt user for positve int
+    while(i == 1)
+    {
+        
+        printf("Enter a positive integer: ");
+        int prompt = scanf("%i", &user_input);
+        if(prompt != 1)
         {
-
-                printf("Enter a positive integer: ");
-                int prompt = scanf("%i", &user_input); 
-                if(prompt != 1)
-                {
-
-                        printf("Error! Please enter only positive integers.\n");
-                        i = 0;
-                }
-                else if(user_input == -1)
-                {
-                        i = 0;
-                }
-                else
-                {
-                        largest_int = fmax(user_input, largest_int);
-                        // set smallest to first entered number otherwise it will
-                        // always be 0
-                        if(num_of_ints == 0)
-                        {
-                                smallest_int = user_input;
-                        }
-                        smallest_int = fmin(user_input, smallest_int);
-                        num_of_ints++;
-                }
+            
+            printf("Error! Please enter only positive integers.\n");
+            i = 0;
         }
-
-        printf("Largest: %i \nSmallest: %i \nTotal: %i\n", largest_int, smallest_int, num_of_ints);
-
-        return 0;
+        else if(user_input == -1)
+        {
+            i = 0;
+        }
+        else
+        {
+            largest_int = fmax(user_input, largest_int);
+            // set smallest to first entered number otherwise it will
+            // always be 0
+            if(num_of_ints == 0)
+            {
+                smallest_int = user_input;
+            }
+            smallest_int = fmin(user_input, smallest_int);
+            num_of_ints++;
+        }
+    }
+    
+    printf("Largest: %i \nSmallest: %i \nTotal: %i\n", largest_int, smallest_int, num_of_ints);
+    
+    return 0;
 }
