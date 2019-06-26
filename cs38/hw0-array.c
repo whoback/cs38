2ed1e6f70d40a11bbdf89e9309acef4be5fdb643
Will Hoback <will.hoback@gmail.com>
2019-06-25 22:39:23 -0400

    array refactor to use fgets

--- cs38/hw0-array.c
+++ cs38/hw0-array.c
@@ -2,43 +2,51 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int arr[3];
-int input;
+int arr[100];
+int i;
 enum dir {SMALL, LARGE, COUNT};
 
 int main()
 {
-        while((input = fgetc(stdin)) != '\r' && input != '\n' && input != EOF)
-        {
-                printf("Please enter a positive integer: ");
+    
+    char line[256];
+    int i;
+    while(true)
+    {
+        printf("Please enter a positive integer: ");
+        if (fgets(line, sizeof(line), stdin)) {
+            if (1 == sscanf(line, "%d", &i)) {
+                
                 // -1 entered should just print out results
-                if(input == -1)
+                if(i == -1)
                 {
-                        break;
+                    break;
                 }
-                if(input < -1)
+                if(i < -1)
                 {
-                        printf("Only positive integers please!\n");
-                        break;
+                    printf("Only positive integers please!\n");
+                    break;
                 }
-
+                
                 // set SMALL to the very first value otherwise it will be 0
                 if(arr[COUNT] == 0)
                 {
-                        arr[SMALL] = input;
-                        arr[COUNT] += 1;
+                    arr[SMALL] = i;
+                    arr[COUNT] += 1;
                 }
-                if(input > arr[LARGE])
+                if(i > arr[LARGE])
                 {
-                        arr[LARGE] = input;
-                        arr[COUNT] += 1;
+                    arr[LARGE] = i;
+                    arr[COUNT] += 1;
                 }
-                if(input < arr[SMALL])
+                if(i < arr[SMALL])
                 {
-                        arr[SMALL] = input;
-                        arr[COUNT] += 1;
+                    arr[SMALL] = i;
+                    arr[COUNT] += 1;
                 }
+            }
         }
-        printf("Largest: %i\n Smallest: %i\n Count: %i\n", arr[LARGE], arr[SMALL], arr[COUNT]);
-        return 0;
+    }
+    printf("Largest: %i\n Smallest: %i\n Count: %i\n", arr[LARGE], arr[SMALL], arr[COUNT]);
+    return 0;
 }
