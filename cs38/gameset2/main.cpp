c7809f06a6a1a669598bd08b8e2e9bbd2e2a0f99
Will Hoback <will.hoback@gmail.com>
2019-07-03 23:44:39 -0400

    store used letters and check against input

--- cs38/gameset2/main.cpp
+++ cs38/gameset2/main.cpp
@@ -52,6 +52,18 @@ auto isLetterInWord(std::string word, std::string letter)
     }
     return 0;
 }
+
+auto hasLetterBeenUsed(std::string letterToGuess, std::vector<std::string> usedLetters)
+{
+    std::vector<std::string>::iterator itr = std::find(usedLetters.begin(), usedLetters.end(), letterToGuess);
+    if(itr != usedLetters.end())
+    {
+        // has been used
+        return 0;
+    }
+    // has not been used
+    return 1;
+}
 std::string getUserGuess(std::string input, std::string letterToGuess){
     while((std::cout << "Guess a letter: ")
           && std::getline(std::cin, input))
@@ -82,15 +94,12 @@ int main(int argc, const char * argv[]) {
     // generate a word for the user and change it to ___s
     std::string wordToGuess = randomAccessLibrary(lib);
     std::string underscore = changeToUnderscores(wordToGuess);
-    //int numberOfGuesses = 0;
-    
-    
-    
-    
+    int goodGuesses = 0;
+    int badGuesses = 0;
     std::string input;
     std::string letterToGuess;
     std::vector<int> positions;
-    
+    std::vector<std::string> usedLetters;
     std::cout << "Your word to guess is: " << std::endl;
     std::cout << wordToGuess << std::endl;
     std::cout << underscore << std::endl;
@@ -98,10 +107,11 @@ int main(int argc, const char * argv[]) {
     {
         // get user input
         letterToGuess = getUserGuess(input, letterToGuess);
+        
         // compare letter -> word
-        if(isLetterInWord(wordToGuess, letterToGuess) != 1)
+        if(isLetterInWord(wordToGuess, letterToGuess) != 1 && hasLetterBeenUsed(letterToGuess, usedLetters) != 0)
         {
-            
+            usedLetters.push_back(letterToGuess);
             for(int i = 0; i < wordToGuess.length(); i++)
             {
                 if(wordToGuess[i] == letterToGuess[0])
@@ -110,19 +120,27 @@ int main(int argc, const char * argv[]) {
                     underscore[i] = letterToGuess[0];
                 }
             }
-            std::cout << underscore << std::endl;
+            
 //            std::cout << "size of positions is: " << positions.size() << std::endl;
 //            std::cout << "size of actual word : " << wordToGuess.length()<<std::endl;
             // check if winner
             if(isWinner(positions, wordToGuess) == 0)
             {
+                goodGuesses++; // for the last count
+                std::cout << "\n\n\n\n\n";
                 std::cout << "Winner!" << std::endl;
+                std::cout << "You took " << goodGuesses + badGuesses << " guesses to get it right!" << std::endl;
+                std::cout << goodGuesses << " of them were correct and " << badGuesses << " were incorrect!" << std::endl;
                 break;
             }
+            goodGuesses++;
+            
+            std::cout << underscore << std::endl;
         }
         else
         {
             std::cout << "Oops try another guess!" << std::endl;
+            badGuesses++;
         }
     }
     
