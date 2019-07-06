fa62320a8a384effb9576f6dc9895620c1656cab
Will Hoback <will.hoback@gmail.com>
2019-07-06 16:40:10 -0400

    handle case when user incorrectly guesses entire word

--- cs38/gameset2/main.cpp
+++ cs38/gameset2/main.cpp
@@ -12,6 +12,17 @@
 #include <iterator>
 #include <sstream>
 
+std::string generateRandomWordFromLibrary(std::vector<std::string> lib);
+std::string changeToUnderscores(std::string str);
+bool letterIsInWord(std::string word, std::string letter);
+bool letterAlreadyUsed(std::string letterToGuess, std::vector<std::string> usedLetters);
+std::string getUserGuess(std::string input, std::string letterToGuess);
+bool hasUserGuessedAllLetters(std::vector<int> positions, std::string wordToGuess);
+void printWinningMessage(int goodGuesses, int badGuesses, std::vector<std::string> usedLetters);
+void initWordToGuessMessage(std::string underscore);
+bool doesUserWantToGuessWholeWord();
+std::string getUserWholeWordGuess();
+
 std::vector<std::string> lib = {
     "apple",
     "gnostic",
@@ -28,168 +39,6 @@ std::vector<std::string> lib = {
     "rainbow"
 };
 
-/**
- Generate a random entry from our library of words
-
- @param lib a vector of strings
- @return a string that the user will end up guessing
- */
-std::string generateRandomWordFromLibrary(std::vector<std::string> lib)
-{
-    std::random_device rd;
-    std::mt19937 g(rd());
-    std::shuffle(lib.begin(), lib.end(), g);
-    
-    return lib.front();
-}
-
-/**
- This takes our word and replaces every character with an underscore
-
- @param str a string, this is the word that is being guessed by the user
- @return a new string of all underscores
- */
-std::string changeToUnderscores(std::string str)
-{
-    return str.replace(str.begin(), str.end(), str.length(), '_');
-}
-
-
-/**
- Returns true if the letter is found within the word
-
- @param word word to search
- @param letter letter to search for
- @return bool
- */
-bool letterIsInWord(std::string word, std::string letter)
-{
-    if(word.find(letter))
-    {
-        return true;
-    }
-    return false;
-}
-
-/**
- Returns true if the letter has already been used
-
- @param letterToGuess the letter entered by the user
- @param usedLetters a vector of already entered letters
- @return bool
- */
-bool letterAlreadyUsed(std::string letterToGuess, std::vector<std::string> usedLetters)
-{
-    std::vector<std::string>::iterator itr = std::find(usedLetters.begin(), usedLetters.end(), letterToGuess);
-    if(itr != usedLetters.end())
-    {
-        // has been used
-        return true;
-    }
-    // has not been used
-    return false;
-}
-std::string getUserGuess(std::string input, std::string letterToGuess){
-    while((std::cout << "Guess a letter: ")
-          && std::getline(std::cin, input))
-    {
-        std::istringstream is {input};
-        if((is >> letterToGuess) && !(is >> input) && (letterToGuess.length() < 2))
-        {
-            break;
-        }
-        std::cerr << "Invalid input please try again." << std::endl;
-        
-    }
-    // deal with upcased characters
-    letterToGuess = std::tolower(letterToGuess[0]);
-    return letterToGuess;
-}
-
-bool hasUserGuessedAllLetters(std::vector<int> positions, std::string wordToGuess)
-{
-    if(positions.size() == wordToGuess.length())
-    {
-        return true;
-    }
-    return false;
-}
-
-
-/**
- Prints a message if the user has won the game
-
- @param goodGuesses numer of good guesses the user has currently
- @param badGuesses number of bad guesses the user has currently
- @param usedLetters vector of letters used in all guesses
- */
-void printWinningMessage(int goodGuesses, int badGuesses, std::vector<std::string> usedLetters)
-{
-    const char * wtext = R"(
-    __      __  _                                       _
-    \ \    / / (_)    _ _     _ _      ___      _ _    | |
-     \ \/\/ /  | |   | ' \   | ' \    / -_)    | '_|   |_|
-      \_/\_/  _|_|_  |_||_|  |_||_|   \___|   _|_|_   _(_)_
-    _|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_| """ |
-    "`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'
-    )";
- 
-    std::cout << "\n\n\n\n\n\n\n\n\n";
-    std::cout << wtext << std::endl;
-    std::cout << "You took " << goodGuesses + badGuesses << " guesses to get it right!" << std::endl;
-    std::cout << goodGuesses << " of them were correct and " << badGuesses << " were incorrect!" << std::endl;
-    std::cout << "Look at all the letters you guessed!" << std::endl;
-    for(std::string x : usedLetters)
-    {
-        std::cout << x;
-    };
-}
-
-/**
- Prints out the word for the user to guess as underscores
-
- @param underscore the word to guess replaced with underscores
- */
-void initWordToGuessMessage(std::string underscore)
-{
-    std::cout << "Your word to guess is: " << std::endl;
-    
-    std::cout << underscore << std::endl;
-}
-
-/**
- Returns true if the user wants to guess the entire word and false if not.
-
- @return bool
- */
-bool doesUserWantToGuessWholeWord()
-{
-    std::cout << "Do you want to guess the whole word? Enter Y for yes: ";
-    std::string resp;
-    std::getline(std::cin, resp);
-    resp = std::tolower(resp[0]);
-    if(resp == "y")
-    {
-
-        return true;
-    }
-    return false;
-    
-}
-
-/**
- This function gets the user's guess of the entire word.
-
- @return a string containing the entered guess
- */
-std::string getUserWholeWordGuess()
-{
-    std::cout << "Enter your guess: ";
-    std::string guess;
-    std::getline(std::cin, guess);
-    
-    return guess;
-}
 
 int main(int argc, const char * argv[]) {
     // vars for gameplay
@@ -248,6 +97,9 @@ int main(int argc, const char * argv[]) {
                    printWinningMessage(goodGuesses, badGuesses, usedLetters);
                    break;
                }
+                else{
+                    std::cout << "Sorry... wrong guess!" << std::endl;
+                }
                 
             }
             
@@ -264,6 +116,169 @@ int main(int argc, const char * argv[]) {
     }
     
     return 0;
+}
+
+/**
+ Generate a random entry from our library of words
+ 
+ @param lib a vector of strings
+ @return a string that the user will end up guessing
+ */
+std::string generateRandomWordFromLibrary(std::vector<std::string> lib)
+{
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(lib.begin(), lib.end(), g);
+    
+    return lib.front();
+}
+
+/**
+ This takes our word and replaces every character with an underscore
+ 
+ @param str a string, this is the word that is being guessed by the user
+ @return a new string of all underscores
+ */
+std::string changeToUnderscores(std::string str)
+{
+    return str.replace(str.begin(), str.end(), str.length(), '_');
+}
+
+
+/**
+ Returns true if the letter is found within the word
+ 
+ @param word word to search
+ @param letter letter to search for
+ @return bool
+ */
+bool letterIsInWord(std::string word, std::string letter)
+{
+    if(word.find(letter))
+    {
+        return true;
+    }
+    return false;
+}
+
+/**
+ Returns true if the letter has already been used
+ 
+ @param letterToGuess the letter entered by the user
+ @param usedLetters a vector of already entered letters
+ @return bool
+ */
+bool letterAlreadyUsed(std::string letterToGuess, std::vector<std::string> usedLetters)
+{
+    std::vector<std::string>::iterator itr = std::find(usedLetters.begin(), usedLetters.end(), letterToGuess);
+    if(itr != usedLetters.end())
+    {
+        // has been used
+        return true;
+    }
+    // has not been used
+    return false;
+}
+std::string getUserGuess(std::string input, std::string letterToGuess){
+    while((std::cout << "Guess a letter: ")
+          && std::getline(std::cin, input))
+    {
+        std::istringstream is {input};
+        if((is >> letterToGuess) && !(is >> input) && (letterToGuess.length() < 2))
+        {
+            break;
+        }
+        std::cerr << "Invalid input please try again." << std::endl;
+        
+    }
+    // deal with upcased characters
+    letterToGuess = std::tolower(letterToGuess[0]);
+    return letterToGuess;
+}
+
+bool hasUserGuessedAllLetters(std::vector<int> positions, std::string wordToGuess)
+{
+    if(positions.size() == wordToGuess.length())
+    {
+        return true;
+    }
+    return false;
+}
+
+
+/**
+ Prints a message if the user has won the game
+ 
+ @param goodGuesses numer of good guesses the user has currently
+ @param badGuesses number of bad guesses the user has currently
+ @param usedLetters vector of letters used in all guesses
+ */
+void printWinningMessage(int goodGuesses, int badGuesses, std::vector<std::string> usedLetters)
+{
+    const char * wtext = R"(
+    __      __  _                                       _
+    \ \    / / (_)    _ _     _ _      ___      _ _    | |
+    \ \/\/ /  | |   | ' \   | ' \    / -_)    | '_|   |_|
+    \_/\_/  _|_|_  |_||_|  |_||_|   \___|   _|_|_   _(_)_
+    _|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_| """ |
+    "`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'
+    )";
+    
+    std::cout << "\n\n\n\n\n\n\n\n\n";
+    std::cout << wtext << std::endl;
+    std::cout << "You took " << goodGuesses + badGuesses << " guesses to get it right!" << std::endl;
+    std::cout << goodGuesses << " of them were correct and " << badGuesses << " were incorrect!" << std::endl;
+    std::cout << "Look at all the letters you guessed!" << std::endl;
+    for(std::string x : usedLetters)
+    {
+        std::cout << x;
+    };
+}
+
+/**
+ Prints out the word for the user to guess as underscores
+ 
+ @param underscore the word to guess replaced with underscores
+ */
+void initWordToGuessMessage(std::string underscore)
+{
+    std::cout << "Your word to guess is: " << std::endl;
+    
+    std::cout << underscore << std::endl;
+}
+
+/**
+ Returns true if the user wants to guess the entire word and false if not.
+ 
+ @return bool
+ */
+bool doesUserWantToGuessWholeWord()
+{
+    std::cout << "Do you want to guess the whole word? Enter Y for yes: ";
+    std::string resp;
+    std::getline(std::cin, resp);
+    resp = std::tolower(resp[0]);
+    if(resp == "y")
+    {
+        
+        return true;
+    }
+    return false;
+    
+}
+
+/**
+ This function gets the user's guess of the entire word.
+ 
+ @return a string containing the entered guess
+ */
+std::string getUserWholeWordGuess()
+{
+    std::cout << "Enter your guess: ";
+    std::string guess;
+    std::getline(std::cin, guess);
+    
+    return guess;
 }
 
 
