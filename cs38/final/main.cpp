d4a80f4446131d32753bb3999f877c5d57f1a47c
Will Hoback <will.hoback@gmail.com>
2019-07-19 18:03:32 -0400

    get border_box and player mvmt happening... dont refactor too early

--- cs38/final/main.cpp
+++ cs38/final/main.cpp
@@ -9,7 +9,61 @@
 #include <iostream>
 #include <ncurses.h>
 
+typedef enum {
+    right,
+    left,
+    up,
+    down,
+} direction;
+
+constexpr int MAP_WIDTH = 20;
+constexpr int MAP_HEIGHT = 20;
+
+void move(direction dir){
+    
+}
+
 int main(int argc, const char * argv[]) {
-    std::cout << "hellow rold";
+    initscr(); // ncurses
+    char player = '@';
+    char s = ' ';
+    int xloc = MAP_WIDTH / 2;
+    int yloc = MAP_HEIGHT / 2;
+    //draw border of our room
+    char wall = '#';
+    cbreak(); // don't buffer lines just use input
+    keypad(stdscr, TRUE); // used for arrow keys
+    noecho(); // don't echo char input
+    box(stdscr, wall, wall);
+    mvwaddch(stdscr,xloc,yloc,player);
+    refresh();
+    int ch = 1;
+    // use enter to quit for testing
+    while((ch = getch()) != 10)
+    {
+        if(ch == KEY_RIGHT) yloc++;
+        if(ch == KEY_LEFT) yloc--;
+        if(ch == KEY_DOWN)
+        {
+            
+             if(xloc == getmaxy(stdscr) - 1)
+                 printw("max x");
+            else
+                xloc++;
+            mvwaddch(stdscr,xloc,yloc,player);
+            mvwaddch(stdscr, xloc-1, yloc-1, s);
+        }
+        if(ch == KEY_UP)
+        {
+            if(xloc == getbegy(stdscr) + 1)
+                printw("beg y");
+            else
+            xloc--;
+            mvwaddch(stdscr,xloc,yloc,player);
+            mvwaddch(stdscr, xloc-1, yloc-1, s);
+            refresh();
+        }
+    }
+    endwin();
     return 0;
 }
