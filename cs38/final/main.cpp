816e229f3c9036327de764861f3f0da574caccb9
Will Hoback <will.hoback@gmail.com>
2019-08-06 23:15:23 -0400

    redo project layout and get multiple windows happening along with movement, basic hud status, walls, and character

--- cs38/final/main.cpp
+++ cs38/final/main.cpp
@@ -7,65 +7,163 @@
 
 #include <iostream>
 #include <ncurses.h>
+#include <string>
+void init();
+void drawmap();
+void end();
+void loop();
+void hud();
 
+
+
+const int MAP_HEIGHT = 40;
+const int MAP_WIDTH = 80;
+
+WINDOW * map;
+WINDOW * disp;
+
+struct player {
+    int y = 0;
+    int x = 0;
+    char sign;
+};
+
+player p;
+
+int startx, starty, width, height;
+// keep track of borders
+int max_y = 0;
+int max_x = 0;
+//char s = ' ';
+int xloc = max_x/2;
+int yloc = max_y/2;
+//draw border of our room
+char wall = '#';
 
 int main(int argc, const char * argv[])
 {
+    
+    init();
+    
+    wmove(map, xloc, yloc);
+    waddch(map, p.sign);
+    wrefresh(map);
+    loop();
+//    int ch;
+//    ch = getch();
+//    if(ch == 'q')
+//        end();
+//    else if(ch == 10)
+//    {
+//        drawmap();
+//    }
+    
+    //drawmap();
+ 
+//    end();
+    
+    return 0;
+}
+
+void init()
+{
     initscr(); // ncurses
-    curs_set(0); // remove default cursor and only show @ icon
-    // give player an icon
-    char player = '@';
-    // keep track of borders
-    int max_y = 0;
-    int max_x = 0;
-    getmaxyx(stdscr, max_y, max_x);
-    //char s = ' ';
-    int xloc = max_x/2;
-    int yloc = max_y/2;
-    //draw border of our room
-    char wall = '#';
-    cbreak(); // don't buffer lines just use input
+    cbreak();
+    noecho();
+    raw();
+    intrflush(stdscr, FALSE);
     keypad(stdscr, TRUE); // used for arrow keys
     noecho(); // don't echo char input
-    box(stdscr, wall, wall);
-    mvwaddch(stdscr,xloc,yloc,player);
-    refresh();
-    int ch = 1;
-    // use enter to quit for testing
-    while((ch = getch()) != 10)
+    curs_set(0); // remove default cursor and only show @ icon
+    map = newwin(MAP_HEIGHT, MAP_WIDTH, 0, 0);
+    disp  = newwin(5, MAP_WIDTH, MAP_HEIGHT+5, 0);
+    getmaxyx(map, max_y, max_x);
+    // give player an icon
+    p.sign = '@';
+    p.x = xloc;
+    p.y = yloc;
+    wrefresh(map);
+    
+}
+
+void drawmap()
+{
+    
+    wborder(map, '|', '|', '-', '-', '+', '+', '+', '+');
+    wmove(map, p.y, p.x);
+    waddch(map, p.sign);
+    wrefresh(map);
+}
+
+void end()
+{
+    clear();
+    delwin(map);
+    endwin();
+    exit(0);
+}
+
+void loop()
+{
+    while(1)
     {
+        drawmap();
+        hud();
+        wnoutrefresh(map);
+        wnoutrefresh(disp);
+        doupdate();
+        int ch = getch();
         if(ch == KEY_RIGHT)
         {
-            xloc++;
-            if(xloc > max_x - 2) // -2 for the wall
-                xloc = max_x - 2;
-            mvwaddch(stdscr,yloc,xloc,player);
+            p.x++;
+            if(p.x > max_x - 2) // -2 for the wall
+                p.x = max_x - 2;
+//            wmove(map, p.y, p.x);
+//            waddch(map, p.sign);
+            
             
         }
         if(ch == KEY_LEFT)
         {
-            xloc--;
-            if(xloc < 1)
-                xloc = 1;
-            mvwaddch(stdscr,yloc,xloc,player);
+            p.x--;
+            if(p.x < 1)
+                p.x = 1;
+//            wmove(map, p.y, p.x);
+//            waddch(map, p.sign);
         }
         if(ch == KEY_DOWN)
         {
-            yloc++;
-            if(yloc > max_y-2)
-                yloc = max_y-2;
-            mvwaddch(stdscr,yloc,xloc,player);
+            p.y++;
+            if(p.y > max_y-2)
+                p.y = max_y-2;
+//            wmove(map, p.y, p.x);
+//            waddch(map, p.sign);
         }
         if(ch == KEY_UP)
         {
-            yloc--;
-            if(yloc < 1)
-                yloc = 1;
-            mvwaddch(stdscr,yloc,xloc,player);
-            
-            refresh();
+            p.y--;
+            if(p.y < 1)
+                p.y = 1;
+//            wmove(map, p.y, p.x);
+//            waddch(map, p.sign);
+        }
+        if (ch == 'Q' || ch == 'q')
+        {
+            end();
         }
     }
-    endwin();
-    return 0;
+}
+
+void hud()
+{
+    std::string x = std::to_string(p.x);
+    std::string y = std::to_string(p.y);
+    werase(disp);
+    
+    wmove(disp, 0, 0);
+    
+    waddstr(disp, "Current coordinates: ");
+    waddstr(disp, x.c_str());
+    waddstr(disp, ", ");
+    waddstr(disp, y.c_str());
 }
