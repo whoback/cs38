01b2311e3870bf193ffefc54878e43d5e3fcc8ba
Will Hoback <will.hoback@gmail.com>
2019-08-07 01:07:55 -0400

    resize to make map more manageable, remove unnecessary struct, add some helper functions

--- cs38/final/main.cpp
+++ cs38/final/main.cpp
@@ -13,63 +13,61 @@
 
 void init();
 void initchars();
+void initmapandhud();
 void drawmap();
 void end();
 void loop();
-void hud();
+void displayhud();
+void introtext();
 
 
-
-const int MAP_HEIGHT = 40;
-const int MAP_WIDTH = 80;
+const int MAP_HEIGHT = 20;
+const int MAP_WIDTH = 40;
 
 WINDOW * map;
-WINDOW * disp;
+WINDOW * hud;
 
 struct player {
     std::string name;
     int y = 0;
     int x = 0;
     char sign;
+    int turn = 0;
     int gold = 100;
 };
 
-struct storekeep {
-    std::string name;
-    int y = 0;
-    int x = 0;
-    char sign;
-    int gold = 0;
-};
-
 player p;
-storekeep shop;
+player shop;
 
+//use for all our random number needs
 std::random_device rd;
 auto rdval = rd();
 std::mt19937 ran(rdval);
 auto ranval = ran();
 
+std::array<std::string, 3> items = {"Spellbooks", "Magic Items", "Coffee"};
 
+//possible shopekeeper names some created by me and some taken from nethack
 std::array<std::string, 25> names = {"Uze Bandi", "Bebe Pitolito", "Habibi Habeeb", "Njezjin", "Tsjernigof", "Ossipewsk", "Gorlowka", "Gomel", "Konosja", "Weliki Oestjoeg", "Syktywkar", "Sablja", "Narodnaja", "Kyzyl", "Walbrzych", "Swidnica", "Klodzko", "Raciborz", "Gliwice", "Brzeg", "Krnov", "Hradec Kralove", "Leuk", "Brig", "Sarnen"};
 
 int startx, starty, width, height;
 // keep track of borders
 int max_y = 0;
 int max_x = 0;
-//char s = ' ';
-int xloc = (max_x / 2);
-int yloc = (max_y / 2);
+
 //draw border of our room
 char wall = '#';
 
 int main(int argc, const char * argv[])
 {
-    
+    //start ncurses and show intro text to user
     init();
+    
+    introtext();
+    initmapandhud();
     initchars();
     
-    wmove(map, xloc, yloc);
+    wmove(map, max_x/2, 0);
     waddch(map, p.sign);
     wrefresh(map);
     loop();
@@ -86,12 +84,6 @@ void init()
     keypad(stdscr, TRUE); // used for arrow keys
     noecho(); // don't echo char input
     curs_set(0); // remove default cursor and only show @ icon
-    map = newwin(MAP_HEIGHT, MAP_WIDTH, 0, 0);
-    disp  = newwin(5, MAP_WIDTH, MAP_HEIGHT+5, 0);
-    getmaxyx(map, max_y, max_x);
-    // give player an icon
- 
-    wrefresh(map);
     
 }
 
@@ -115,6 +107,7 @@ void end()
 {
     clear();
     delwin(map);
+    delwin(hud);
     endwin();
     exit(0);
 }
@@ -123,11 +116,14 @@ void loop()
 {
     while(1)
     {
+        //advance our turn
+        ++p.turn;
         drawmap();
-        hud();
+        displayhud();
         wnoutrefresh(map);
-        wnoutrefresh(disp);
+        wnoutrefresh(hud);
         doupdate();
+        
         int ch = getch();
         if(ch == KEY_RIGHT)
         {
@@ -157,38 +153,41 @@ void loop()
         {
             end();
         }
+        if(ch == '.' || ch == ' ')
+        {
+            // don't do anything this is just like nethack's commands
+        }
     }
 }
 
-void hud()
+void displayhud()
 {
-    std::string x = std::to_string(p.x);
-    std::string y = std::to_string(p.y);
-    std::string locs = std::to_string(max_x/2);
-    locs += std::to_string(max_y/2);
+    //clear hud
+    werase(hud);
+    //move to (x,y) of hud window
+    wmove(hud, 0, 0);
+    //print player location
+    waddstr(hud, "Location: ");
+    waddstr(hud, std::to_string(p.x).c_str());
+    waddstr(hud, ", ");
+    waddstr(hud, std::to_string(p.y).c_str());
+    waddch(hud, '\n');
+    
+    //print who our shopkeeper is
+    waddstr(hud, "Shop owned by: ");
+    waddstr(hud, shop.name.c_str());
+    waddch(hud, '\n');
+    
+    //print turn number
+    waddstr(hud, "Turn: ");
+    waddstr(hud, std::to_string(p.turn).c_str());
     
     
-    werase(disp);
-    
-    wmove(disp, 0, 0);
-    
-    waddstr(disp, "Current coordinates: ");
-    waddstr(disp, x.c_str());
-    waddstr(disp, ", ");
-    waddstr(disp, y.c_str());
-    waddch(disp, '\n');
-    waddstr(disp, "Shopkeeper name: ");
-    waddstr(disp, shop.name.c_str());
-    waddch(disp, '\n');
-    waddstr(disp, "XYLocs: ");
-    waddstr(disp, locs.c_str());
 }
 
 void initchars()
 {
     p.sign = '@';
-    p.x = xloc;
-    p.y = yloc;
     p.gold = 100;
     shop.sign = '&';
     shop.x = max_x / 2;
@@ -196,4 +195,36 @@ void initchars()
     shop.name = names.at(ranval%25);
     shop.gold = 5000;
     
+}
+
+void introtext()
+{
+    printw("Welcome! \n Some quick information: \n 1. To move use the arrow keys\n 2. Quit at anytime by pressing q\n 3. Have fun!");
+    printw("Do you want to play? y or n...");
+    // this should be printed in the stdscrn before we create our map and hud
+    while(1)
+    {
+        int ch = getch();
+        if(ch == 'y')
+        {
+            
+            break;
+        }
+        if(ch == 'n' || ch == 'q')
+        {
+            end();
+        }
+
+    }
+    
+}
+
+void initmapandhud()
+{
+    map = newwin(MAP_HEIGHT, MAP_WIDTH, 0, 0);
+    hud  = newwin(5, MAP_WIDTH, MAP_HEIGHT+5, 0);
+    getmaxyx(map, max_y, max_x);
+    // give player an icon
+    
+    wrefresh(map);
 }
